514b6d1d17eabd0527b8ef17ae5d4856b993ac01
Husni <husni12011980@gmail.com>
2025-11-07 14:01:46 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-  document.querySelectorAll('.fade-scroll').forEach(el => {
-    const rect = el.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      el.classList.add('visible');
-    }
-  });
-});
